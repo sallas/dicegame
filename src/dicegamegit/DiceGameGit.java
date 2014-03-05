ed4af4be2f00c8d8134594982294363039aab66b
Tobias karlsson <tbc105@hotmail.com>
2014-03-05 15:57:16 +0100

    Removed comments

--- src/dicegamegit/DiceGameGit.java
+++ src/dicegamegit/DiceGameGit.java
@@ -1,21 +1,11 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package dicegamegit;
 
-/**
- *
- * @author tobbe
- */
 public class DiceGameGit
 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args)
     {
-        // TODO code application logic here
+        int mine = 1;
     }
 }
